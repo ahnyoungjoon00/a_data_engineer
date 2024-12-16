014261cebc062afc07632e6cbc71c55056fa9fb0
ahn young joon <ahnyj1229@gmail.com>
2024-12-17 00:08:53 +0900

    subject Django
    description
    장고를 활용해서 웹에서
    로그인페이지 구성(내장, custom)
    회원가입페이지 구성(내장, custom)

--- h_django/test_project/static/js/index.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){ // 브라우저 창이 모두 뜨고나면, 익명함수(function(){})를 실행할 것이라는 의미
-    let btn = document.getElementById("btn"); // id가 btn인 태그 객체를 btn변수에 저장
-    btn.addEventListener("click", show_msg); // btn객체에서 클릭이벤트가 발생하면 show_msg 함수를 실행하라
-};
-
-function show_msg(){
-    alert("msg 출력"); // 확인버튼이 잇는 메시지창을 출력하게 된다.
-}
--- h_django/texas/Scripts/activate.bat
+++ h_django/texas/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\DjangoWorkspace\texas
+set VIRTUAL_ENV=C:\Dev\git_ahnlab\a_data_engineer\h_django\texas
 
 if not defined PROMPT set PROMPT=$P$G
 
